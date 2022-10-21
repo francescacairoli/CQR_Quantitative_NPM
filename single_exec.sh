d008039e7f6026cb527b772bd17b29138b34325f
francescacairoli <francescacairoli@yahoo.it>
2022-10-21 11:20:04 +0200

    AAD and EHT added

--- single_exec.sh
+++ single_exec.sh
@@ -4,9 +4,6 @@
 # settings #
 ############
 
-# model dim = 2 nb comb = 1
-# model dim = 4 nb comb = 6
-# model dim = 8 nb comb = 28
 MODEL_PREFIX="MRH" 
 MODEL_DIM=4 
 
@@ -16,13 +13,13 @@ LOGS="out/logs/"
 mkdir -p $LOGS
 OUT="${LOGS}${DATE}_${TIME}_out.txt"
 
-NB_COMB=6
+
 #######
 # run #
 #######
 
-for i in 0 1 2 3 4 5 
+for i in 0 1 2 3
 do
 	echo i: $i
-	python comb_stoch_run_cqr.py --comb_idx $i --model_prefix $MODEL_PREFIX --model_dim $MODEL_DIM >> $OUT 2>&1
+	python stoch_run_cqr.py --property_idx $i --model_prefix $MODEL_PREFIX --model_dim $MODEL_DIM >> $OUT 2>&1
 done
